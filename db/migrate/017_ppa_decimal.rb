37b58dca0a7f024a30473a553b7542b5e26b9c19
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-04-25 19:14:57 +0000

    Changed PPA fields to decimals.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@184 55dd9f42-8528-0410-b227-b9e89acb4f78

---
+++ db/migrate/017_ppa_decimal.rb
@@ -0,0 +1,12 @@
+class PpaDecimal < ActiveRecord::Migration
+  def self.up
+    change_column :sales_reports, :store_ppa, :decimal, :precision => 8, :scale => 2
+    remove_column :sales_reports, :actual_vs_goal_diff_for_day
+    remove_column :manager_reports, :actual_vs_goal_diff_for_month
+  end
+
+  def self.down
+    change_column :sales_reports, :store_ppa, :integer
+    remove_column :sales_reports, :actual_vs_goal_diff_for_day, :decimal, :precision => 8, :scale => 2
+  end
+end
