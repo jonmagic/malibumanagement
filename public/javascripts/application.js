8dbfbd2e654f67ad21a7ba2ac64c71e4568d1b9e
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-04-26 17:22:48 +0000

    Adding number_field form helper for number-only fields.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@196 55dd9f42-8528-0410-b227-b9e89acb4f78

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 function numbersonly(e){
 	var unicode=e.charCode? e.charCode : e.keyCode
-	if (unicode!=8 && unicode!=9 && unicode!=16 && unicode!=17 && unicode!=37 && unicode!=39 && unicode!=110 && unicode!=190 && unicode!=35 && unicode!=36) { //if the key isn't the backspace key (which we should allow)
+	if (unicode!=8 && unicode!=9 && unicode!=16 && unicode!=17 && unicode!=35 && unicode!=36 && unicode!=37 && unicode!=39 && unicode!=190) { //allow all these keys: 8=backspace; 9=tab; 16=shift; 17=ctrl; 35=end; 36=home; 37=left; 39=right; 190=period
 		if (unicode<48||unicode>57) //if not a number
 			return false //disable key press
 	}
