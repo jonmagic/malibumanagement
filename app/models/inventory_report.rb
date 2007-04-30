86e4dff0e21984a6710a49e44af22e1c9830602e
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-04-30 14:42:57 +0000

    Fixed choking on nil helios inventory description field.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@220 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/inventory_report.rb
+++ app/models/inventory_report.rb
@@ -14,7 +14,7 @@ class InventoryReport < ActiveRecord::Base
       theitems = self.inventory_from_odbc
       return @the_inventory_items unless theitems
       theitems.each do |line_item|
-        next if line_item.nil?
+        next if line_item['Descriptions'].nil?
         self.inventory_line_items.build(:name => line_item['Descriptions'].columnize, :label => line_item['Descriptions'], :should_be => line_item['qty_onhand']) unless self.inventory_line_item(line_item['Descriptions'])
       end
       self.save
