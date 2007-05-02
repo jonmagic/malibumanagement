d8a0151b4d8b39ef22a17dbf5e13a5bab6ed583d
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-05-02 16:45:25 +0000

    Fixed no-submit when assigned_to is selected for managers.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@224 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -113,12 +113,11 @@ logger.error "Current Model: #{current_form_model}"
       @form = FormInstance.find_by_id(params[:form_id])
       return redirect_to(store_dashboard_url) unless @form
 
-      if !params[:form_instance].nil? && !params[:form_instance][:assigned_to].blank?
-        assigned_to_changed = true unless @form.user_id != params[:form_instance][:assigned_to]
+      if !params[:form_instance].nil? && !params[:form_instance][:assigned_to].blank? && @form.user_id != params[:form_instance][:assigned_to]
+        assigned_to_changed = true
         @form.user = User.find_by_id(params[:form_instance][:assigned_to])
         params[:form_instance].delete(:status)
-      end
-      if !params[:form_instance].nil? &&
+      elsif !params[:form_instance].nil? &&
           !params[:form_instance][:status].blank? &&
           !(params[:form_instance][:status].as_status.number == @form.status.as_status.number)
         @form.status = params[:form_instance].delete(:status)
