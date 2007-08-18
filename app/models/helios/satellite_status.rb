b33a6bb8efed38360e3c684d6ccf1aeb31de6f75
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-08-18 19:07:29 +0000

    A few little changes
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@395 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/helios/satellite_status.rb
+++ app/models/helios/satellite_status.rb
@@ -1,6 +1,6 @@
 class Helios::SatelliteStatus < ActiveRecord::Base
   def write_attribute(att, value)
     super
-    save
+    save unless new_record? # Very important, otherwise this goes into an infinite loop when creating new records!
   end
 end
