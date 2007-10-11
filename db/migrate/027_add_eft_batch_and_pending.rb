791d7b79d3a419d86c1e3123a82847bc0e031715
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-10-11 13:23:01 +0000

    Added migration.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@511 55dd9f42-8528-0410-b227-b9e89acb4f78

---
+++ db/migrate/027_add_eft_batch_and_pending.rb
@@ -0,0 +1,25 @@
+class AddEftBatchAndPending < ActiveRecord::Migration
+  def self.up
+    create_table :eft_batches do |t|
+      t.column :for_month, :string # month #1-12 == "Time#year(2007)/Time#month(10)"
+      t.column :submitted_at, :datetime
+      t.column :submitted_by, :integer
+      t.column :eft_count, :integer
+      t.column :eft_total, :integer
+      t.column :eft_count_by_location, :string, :default => {}.to_yaml
+      t.column :eft_count_by_amount, :string, :default => {}.to_yaml
+      t.column :eft_total_by_location, :string, :default => {}.to_yaml
+      t.column :memberships_without_efts, :integer
+      t.column :members_with_expired_cards, :integer
+    end
+    create_table :eft_pendings do |t|
+      t.column :eft_batch_id, :integer
+      t.column :client_profile_id, :integer
+      t.column :amount, :integer # Number stored in cents
+    end
+  end
+  def self.down
+    drop_table :eft_batches
+    drop_table :eft_pendings
+  end
+end
