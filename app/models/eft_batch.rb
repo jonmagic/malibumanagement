52cfd1b87dbdc713928b20bc72af35aac946ba1b
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-10-20 18:31:34 +0000

    Enhanced the EFT memberships processing for custom methods.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@533 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/eft_batch.rb
+++ app/models/eft_batch.rb
@@ -27,39 +27,31 @@ class EftBatch < ActiveRecord::Base
     locations_amounts = {}
     locations_count = {}
     amounts_count = {}
-    sql = ''
-    case ::RAILS_ENV
-    when 'development'
-      sql = "(Member1 = 'VIP' AND '"+Time.parse(month).strftime("%Y-%m-%d")+"' >= Member1_Beg AND Member1_Exp >= '"+Time.parse(month).strftime("%Y-%m-%d")+"') OR (Member2 = 'VIP' AND '"+Time.parse(month).strftime("%Y-%m-%d")+"' >= Member2_Beg AND Member2_Exp >= '"+Time.parse(month).strftime("%Y-%m-%d")+"')"
-    when 'production'
-      sql = "([Member1] = 'VIP' AND '"+Time.parse(month).strftime("%Y%m%d")+"' >= [Member1_Beg] AND [Member1_Exp] >= '"+Time.parse(month).strftime("%Y%m%d")+"') OR ([Member2] = 'VIP' AND '"+Time.parse(month).strftime("%Y%m%d")+"' >= [Member2_Beg] AND [Member2_Exp] >= '"+Time.parse(month).strftime("%Y%m%d")+"')"
-    end
 
-    Helios::ClientProfile.find(:all, :conditions => [sql]).each do |cp|
+    Helios::Eft.memberships(month, true) do |cp|
       if cp.eft.nil?
         @missing_efts << [cp.id.to_i]
       else
-        if(!((!cp.eft.Freeze_Start.nil? ? cp.eft.Freeze_Start.to_date <= Time.parse(month).to_date : false) && (!cp.eft.Freeze_End.nil? ? Time.parse(month).to_date <= cp.eft.Freeze_End.to_date : false)) && ((!cp.eft.Start_Date.nil? ? cp.eft.Start_Date.to_date <= Time.parse(month).to_date : true) && (!cp.eft.End_Date.nil? ? Time.parse(month).to_date <= cp.eft.End_Date.to_date : true)))
-          if(cp.eft.Acct_Exp.nil? ? false : Time.parse(cp.eft.Acct_Exp.sub(/\//, '/01/')) < (Time.now+1.day).beginning_of_day) # Check credit card expiration .. does it expire before tomorrow?
-            @invalid_efts << [cp.id.to_i,'Expired Card']
+        if(cp.eft.Acct_Exp.nil? ? false : Time.parse(cp.eft.Acct_Exp.sub(/\//, '/01/')) < (Time.now+1.day).beginning_of_day) # Check credit card expiration .. does it expire before tomorrow?
+          @invalid_efts << [cp.id.to_i,'Expired Card']
+        else
+          if cp.eft.Acct_Exp.blank? && cp.eft.Bank_ABA.blank?
+            @invalid_efts << [cp.id.to_i,'No Routing Number']
+          elsif cp.eft.Acct_Exp.blank? && !cp.eft.Bank_ABA.blank? && !validABA?(cp.eft.Bank_ABA)
+            @invalid_efts << [cp.id.to_i,'Invalid Routing Number']
+          elsif cp.eft.Acct_No.blank?
+            @invalid_efts << [cp.id.to_i,'No '+(cp.eft.credit_card? ? 'Credit Card' : 'Bank Account')+' Number']
+          elsif cp.eft.credit_card? && !cp.eft.Acct_No.blank? && !validCreditCardNumber?(cp.eft.Acct_No)
+            @invalid_efts << [cp.id.to_i,'Invalid Credit Card Number']
           else
-            if cp.eft.Acct_Exp.blank? && cp.eft.Bank_ABA.blank?
-              @invalid_efts << [cp.id.to_i,'No Routing Number']
-            elsif cp.eft.Acct_Exp.blank? && !cp.eft.Bank_ABA.blank? && !validABA?(cp.eft.Bank_ABA)
-              @invalid_efts << [cp.id.to_i,'Invalid Routing Number']
-            elsif cp.eft.Acct_No.blank?
-              @invalid_efts << [cp.id.to_i,'No '+(cp.eft.credit_card? ? 'Credit Card' : 'Bank Account')+' Number']
-            elsif cp.eft.credit_card? && !cp.eft.Acct_No.blank? && !validCreditCardNumber?(cp.eft.Acct_No)
-              @invalid_efts << [cp.id.to_i,'Invalid Credit Card Number']
-            else
-              # ['AccountID', 'FirstName', 'LastName', 'BankName', 'BankRoutingNumber', 'BankAccountNumber', 'NameOnCard', 'CreditCardNumber', 'Expiration', 'Amount', 'Type', 'AccountType, 'Authorization']
-              location_code = cp.eft.Location || '00'+cp.eft.Client_No.to_s[0,1]
-              location_str = HELIOS_LOCATION_CODES[location_code]
-              if(location_str.blank?)
-                ActionController::Base.logger.info("EFT ##{cp.eft.id} has unknown location code of #{location_code}!")
-                location_str = location_code
-              end
-              amount_int = (cp.eft.Monthly_Fee.to_f*100).to_i
+            # ['AccountID', 'FirstName', 'LastName', 'BankName', 'BankRoutingNumber', 'BankAccountNumber', 'NameOnCard', 'CreditCardNumber', 'Expiration', 'Amount', 'Type', 'AccountType, 'Authorization']
+            location_code = cp.eft.Location || '00'+cp.eft.Client_No.to_s[0,1]
+            location_str = HELIOS_LOCATION_CODES[location_code]
+            if(location_str.blank?)
+              ActionController::Base.logger.info("EFT ##{cp.eft.id} has unknown location code of #{location_code}!")
+              location_str = location_code
+            end
+            amount_int = (cp.eft.Monthly_Fee.to_f*100).to_i
 
 # Acct_Type
 # A => American Express
@@ -78,35 +70,32 @@ class EftBatch < ActiveRecord::Base
 # V == exp:2586, noexp:0,    aba:17 (2569)
 
 # Should we be using cp.eft.Client_Name for the credit_card_name?
-              @members << [
-                cp.id.to_i, # AccountID
-                cp.eft.First_Name, # FirstName
-                cp.eft.Last_Name, # LastName
-                cp.eft.Bank_Name, # BankName
-                cp.eft.Bank_ABA, # BankRoutingNumber
-                cp.eft.credit_card? ? nil : cp.eft.Acct_No, # BankAccountNumber
-                cp.eft.First_Name.to_s + ' ' + cp.eft.Last_Name.to_s, # NameOnCard
-                cp.eft.credit_card? ? cp.eft.Acct_No : nil, # CreditCardNumber
-                cp.eft.Acct_Exp.to_s.gsub(/\d/,''), # Expiration MMYY
-                cp.eft.Monthly_Fee.to_f, # Amount
-                cp.eft.credit_card? ? 'Credit Card' : 'ACH', # Type
-                cp.eft.Acct_Type,
-                'Written' # Authorization
-              ]
-              total_amount += amount_int
+            @members << [
+              cp.id.to_i, # AccountID
+              cp.eft.First_Name, # FirstName
+              cp.eft.Last_Name, # LastName
+              cp.eft.Bank_Name, # BankName
+              cp.eft.Bank_ABA, # BankRoutingNumber
+              cp.eft.credit_card? ? nil : cp.eft.Acct_No, # BankAccountNumber
+              cp.eft.First_Name.to_s + ' ' + cp.eft.Last_Name.to_s, # NameOnCard
+              cp.eft.credit_card? ? cp.eft.Acct_No : nil, # CreditCardNumber
+              cp.eft.Acct_Exp.to_s.gsub(/\d/,''), # Expiration MMYY
+              cp.eft.Monthly_Fee.to_f, # Amount
+              cp.eft.credit_card? ? 'Credit Card' : 'ACH', # Type
+              cp.eft.Acct_Type,
+              'Written' # Authorization
+            ]
+            total_amount += amount_int
 
-              locations_amounts[location_str] ||= 0
-              locations_amounts[location_str] += amount_int
+            locations_amounts[location_str] ||= 0
+            locations_amounts[location_str] += amount_int
 
-              locations_count[location_str] ||= 0
-              locations_count[location_str] += 1
+            locations_count[location_str] ||= 0
+            locations_count[location_str] += 1
 
-              amounts_count[amount_int] ||= 0
-              amounts_count[amount_int] += 1
-            end
+            amounts_count[amount_int] ||= 0
+            amounts_count[amount_int] += 1
           end
-        else
-          # @froze << cp.id.to_i
         end
       end
     end
--- app/models/helios/eft.rb
+++ app/models/helios/eft.rb
@@ -24,6 +24,26 @@ class Helios::Eft < ActiveRecord::Base
   
   include HeliosPeripheral
 
+  def self.memberships(month, render_nils=false)
+    sql = case ::RAILS_ENV
+    when 'development'
+      "(Member1 = 'VIP' AND '"+Time.parse(month).strftime("%Y-%m-%d")+"' >= Member1_Beg AND Member1_Exp >= '"+Time.parse(month).strftime("%Y-%m-%d")+"') OR (Member2 = 'VIP' AND '"+Time.parse(month).strftime("%Y-%m-%d")+"' >= Member2_Beg AND Member2_Exp >= '"+Time.parse(month).strftime("%Y-%m-%d")+"')"
+    when 'production'
+      "([Member1] = 'VIP' AND '"+Time.parse(month).strftime("%Y%m%d")+"' >= [Member1_Beg] AND [Member1_Exp] >= '"+Time.parse(month).strftime("%Y%m%d")+"') OR ([Member2] = 'VIP' AND '"+Time.parse(month).strftime("%Y%m%d")+"' >= [Member2_Beg] AND [Member2_Exp] >= '"+Time.parse(month).strftime("%Y%m%d")+"')"
+    end
+    
+    Helios::ClientProfile.find(:all, :conditions => [sql]).each do |cp|
+      if cp.eft.nil?
+        yield cp if render_nils && block_given?
+      else
+        if(!((!cp.eft.Freeze_Start.nil? ? cp.eft.Freeze_Start.to_date <= Time.parse(month).to_date : false) && (!cp.eft.Freeze_End.nil? ? Time.parse(month).to_date <= cp.eft.Freeze_End.to_date : false)) && ((!cp.eft.Start_Date.nil? ? cp.eft.Start_Date.to_date <= Time.parse(month).to_date : true) && (!cp.eft.End_Date.nil? ? Time.parse(month).to_date <= cp.eft.End_Date.to_date : true)))
+          yield cp if block_given?
+        end
+      end
+    end
+    
+  end
+
   def self.delete_these(ids)
     self.update_satellites = true
     failed_list = []
