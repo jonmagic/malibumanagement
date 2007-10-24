d6f1f7fdb98d3d25c5112275cc9ea036132b56c8
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-10-24 00:05:56 +0000

    fixed the before_save populator
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@555 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/helios/transact.rb
+++ app/models/helios/transact.rb
@@ -21,9 +21,9 @@ class Helios::Transact < ActiveRecord::Base
   set_primary_key 'transact_no'
 
   validates_presence_of :OTNum, :ticket_no
-  before_save do
-    self.OTNum = self.class.next_OTNum
-    self.ticket_no = self.class.next_ticket_no
+  before_save do |record|
+    record.OTNum = record.class.next_OTNum
+    record.ticket_no = record.class.next_ticket_no
   end
 
   include HeliosPeripheral
