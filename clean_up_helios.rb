7b5fe49651936da9234b56d268b428049096e6b8
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-02 06:36:27 +0000

    Minor changes, leading up to 1) possibly creating a mysql version, and 2) adding some core extension methods.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@687 55dd9f42-8528-0410-b227-b9e89acb4f78

---
+++ clean_up_helios.rb
@@ -0,0 +1,14 @@
+
+# 1) Look for transaction
+#   +) If transaction exists
+#     -) are there more than one?
+#       .) yes - delete older ones
+#     -) EDIT transaction
+#   +) If transaction doesn't exist
+#     -) CREATE transaction
+#   +) Record transaction number to csv (log it)
+# 2) Look for Notes
+#   +) If exist and shouldn't, remove
+#   +) If don't exist and should, create
+# 3) Notice the balance and payment amount, report if odd.
+
