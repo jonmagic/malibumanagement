f539adc3f650dd33599afeb41bc5743df94dfeaf
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-06 19:22:22 +0000

    Note updating on master.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@709 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/helios/note.rb
+++ app/models/helios/note.rb
@@ -43,7 +43,7 @@ class Helios::Note < ActiveRecord::Base
     t.id
   end
   def update_on_master(attrs)
-    self.class.update_on_master({:Rec_no => self.id, :Client_no => self.Client_no}.merge(attrs))
+    self.class.update_on_master({:Client_no => self.Client_no}.merge(attrs))
   end
   def delete_from_master
     self.update_on_master(:Deleted => true)
