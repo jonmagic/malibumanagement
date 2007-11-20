2d33aff18ba95ca1dfc27633195d8688d730b76c
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-20 19:41:41 +0000

    Added midnight running of global regenerate.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@760 55dd9f42-8528-0410-b227-b9e89acb4f78

--- regenerate_daemon.rb
+++ regenerate_daemon.rb
@@ -3,4 +3,7 @@ begin
     location = batch.regenerate_now == 'all' ? nil : batch.regenerate_now
     batch.generate(location)
   end
-end while sleep(60) # Wait two minutes between checks.
+  if Time.now.to_i - Time.today.to_i < 360 # within 6 minutes of midnight -- should catch this and should happen only once.
+    EftBatch.find_or_create_by_for_month((Time.now.strftime("%Y").to_i + Time.now.strftime("%m").to_i/12).to_i.to_s + '/' + Time.now.strftime("%m").to_i.cyclical_add(1, 1..12).to_s).generate
+  end
+end while sleep(60) # Wait one minutes between checks.
