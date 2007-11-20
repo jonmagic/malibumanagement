abc89e4ac0d82fa2f1a48658d9863eb2275e4d5e
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-20 18:17:36 +0000

    Fix again.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@754 55dd9f42-8528-0410-b227-b9e89acb4f78

--- regenerate_daemon.rb
+++ regenerate_daemon.rb
@@ -1,6 +1,6 @@
 begin
   if batch = EftBatch.find(:first, :conditions => ['regenerate_now IS NOT NULL'])
     location = batch.regenerate_now == 'all' ? nil : batch.regenerate_now
-    batch.regenerate(location)
+    batch.generate(location)
   end
 end while sleep(60) # Wait two minutes between checks.
