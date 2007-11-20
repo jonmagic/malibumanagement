5bc150b189fc93d7909b7fec878cdf68b141813a
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-20 17:09:20 +0000

    Enhanced is_searchable.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@746 55dd9f42-8528-0410-b227-b9e89acb4f78

--- vendor/plugins/is_searchable/lib/is_searchable.rb
+++ vendor/plugins/is_searchable/lib/is_searchable.rb
@@ -26,8 +26,6 @@ module IsSearchable
         end
       end
       def search(query, options={})
-        limit = options[:limit] || 10
-        offset = options[:offset] || 0
         filters = options[:filters] || {}
 # ActionController::Base.logger.info(options[:filters].inspect)
         if filters.keys.sort == (filters.keys.sort - @method_filters.keys)
@@ -50,17 +48,20 @@ module IsSearchable
       end
       def _bulk_search(query, options={})
         @search_caches ||= {}
-        limit = options[:limit] || 10
+        limit = options[:limit] || 0
         offset = options[:offset] || 0
         filters = options[:filters] || {}
 # ActionController::Base.logger.info(options[:filters].inspect)
 # puts "SELECT #{self.table_name}.* FROM #{self.table_name} #{render_condition_for_query_and_filters(query, filters)} GROUP BY #{self.table_name}.id LIMIT #{limit} OFFSET #{offset}"
 # ActionController::Base.logger.info "SELECT #{self.table_name}.* FROM #{self.table_name} #{render_condition_for_query_and_filters(query, filters)} GROUP BY #{self.table_name}.id LIMIT #{limit} OFFSET #{offset}"
-        @search_caches[query+'_'+filters.values.join('-')] ||= self.find_by_sql("SELECT #{self.table_name}.* FROM #{self.table_name} #{render_condition_for_query_and_filters(query, filters)} GROUP BY #{self.table_name}.id LIMIT #{limit} OFFSET #{offset}")
+        sql = "SELECT #{self.table_name}.* FROM #{self.table_name} #{render_condition_for_query_and_filters(query, filters)} GROUP BY #{self.table_name}.id"
+        sql = sql+" LIMIT #{limit}" if limit > 0
+        sql = sql+" OFFSET #{offset}" if offset > 0
+        @search_caches[query+'_'+filters.values.join('-')] ||= self.find_by_sql(sql)
       end
       def _method_search(query, options={})
         @search_caches ||= {}
-        limit = options[:limit] || 10
+        limit = options[:limit] || 0
         offset = options[:offset] || 0
         filters = options[:filters] || {}
 # ActionController::Base.logger.info(options[:filters].inspect)
@@ -68,29 +69,33 @@ module IsSearchable
 
         begin
           next_rec = self.find_by_sql("SELECT #{self.table_name}.* FROM #{self.table_name} #{render_condition_for_query_and_filters(query, filters)} GROUP BY #{self.table_name}.id LIMIT 1 OFFSET #{results.length}")
-          # Do the method search methods and add the record to results if the methods all return true.
-          results << next_rec
-        end until next_rec.nil? || results.length >= limit
+          results << next_rec if true # Do the method search methods and add the record to results if the methods all return true.
+        end until next_rec.nil? || (limit > 0 && results.length == limit)
 
-        @search_caches[query+'_'+options[:filters].each_value.join('-')] ||= results
+        @search_caches[query+'_'+options[:filters].each_value.join('-')] ||= results[offset-1..results.length]
       end
       def render_condition_for_query_and_filters(query, filters) #search in: first_name, last_name, identifier
 # ActionController::Base.logger.info("Filters in query_and_filters: #{filters.inspect}")
-ActionController::Base.logger.info("**Searching: WHERE (#{self.render_query_condition(query)}) AND (#{self.render_filter_condition(filters)})")
-        "WHERE (#{self.render_query_condition(query)}) AND (#{self.render_filter_condition(filters)})"
+        condition = [
+          query == '' ? nil : "(#{self.render_query_condition(query)})",
+          filters.keys.length == 0 ? nil : "(#{self.render_filter_condition(filters)})"
+        ].compact.join(' AND ')
+ActionController::Base.logger.info("**Search condition: #{condition == '' ? '(none)' : 'WHERE '+condition}")
+        condition == '' ? '' : 'WHERE '+condition
       end
       def render_query_condition(query)
-        self.replace_named_bind_variables(@query_condition, {:query => query, :like_query => '%' + query.to_s + '%'})
+        @query_condition.to_s == '' ? nil : self.replace_named_bind_variables(@query_condition, {:query => query, :like_query => '%' + query.to_s + '%'})
       end
       def render_filter_condition(filters)
 # ActionController::Base.logger.info("Filters in render_filter_condition: #{filters.inspect}")
 # puts "Filters in render_filter_condition: #{filters.inspect}"
 # ActionController::Base.logger.info("@filter_comparisons: #{@filter_comparisons.inspect}")
 # puts "@filter_comparisons: #{@filter_comparisons.inspect}"
-        [1, filters.reject {|f,v| @filter_comparisons[f].nil? }.collect do |key,val|
-            val = "%#{val}%" if @filter_comparisons[key.to_s] =~ /LIKE/
-            self.replace_bind_variables(@filter_comparisons[key.to_s], [val])
-          end].flatten.compact.join(' AND ')
+        condition = [filters.reject {|f,v| @filter_comparisons[f].nil? }.collect do |key,val|
+          val = "%#{val}%" if @filter_comparisons[key.to_s] =~ /LIKE/
+          self.replace_bind_variables(@filter_comparisons[key.to_s], [val])
+        end].flatten.compact.join(' AND ')
+        condition == '' ? nil : condition
       end
     end
   end
