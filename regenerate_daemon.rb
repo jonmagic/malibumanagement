1ae83910d66dfc36dcd0c88fa8e1db73c2fa5068
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-11-30 16:08:19 +0000

    more consistent regenerate nightly - once after 11pm.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@825 55dd9f42-8528-0410-b227-b9e89acb4f78

--- regenerate_daemon.rb
+++ regenerate_daemon.rb
@@ -1,9 +1,11 @@
+last_daily_batch = Time.yesterday
 begin
   if batch = EftBatch.find(:first, :conditions => ['regenerate_now IS NOT NULL AND regenerate_now != ""'])
     location = batch.regenerate_now == 'all' ? nil : batch.regenerate_now
     batch.generate(location)
   end
-  if Time.now.to_i - Time.today.to_i < 360 # within 6 minutes of midnight -- should catch this and should happen only once.
+  if Time.now.hour > 23 # after 11pm -- should catch this and should happen only once.
     EftBatch.find_or_create_by_for_month((Time.now.strftime("%Y").to_i + Time.now.strftime("%m").to_i/12).to_i.to_s + '/' + Time.now.strftime("%m").to_i.cyclical_add(1, 1..12).to_s).generate
+    last_daily_batch = Time.now
   end
 end while sleep(60) # Wait one minutes between checks.
