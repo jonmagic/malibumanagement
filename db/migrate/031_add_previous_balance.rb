d7b83b65cc384fcd459aa463b26d6402eb0ebdd1
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-12-05 02:10:10 +0000

    intermediate migration
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@882 55dd9f42-8528-0410-b227-b9e89acb4f78

--- db/migrate/031_add_previous_balance.rb
+++ db/migrate/031_add_previous_balance.rb
@@ -1,7 +1,7 @@
 class AddPreviousBalance < ActiveRecord::Migration
   def self.up
-    add_column :eft_batches, :previous_balance, :float
-    add_column :eft_batches, :previous_payment_amount, :float
+    add_column :goto_transactions, :previous_balance, :float
+    add_column :goto_transactions, :previous_payment_amount, :float
   end
   def self.down
     remove_column :eft_batches, :previous_balance
