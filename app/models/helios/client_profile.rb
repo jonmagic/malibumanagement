d01448564a8758afe11f5792d2f7552847a68ae5
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-12-12 19:06:03 +0000

    fix weird problem
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@985 55dd9f42-8528-0410-b227-b9e89acb4f78

--- app/models/helios/client_profile.rb
+++ app/models/helios/client_profile.rb
@@ -137,11 +137,11 @@ class Helios::ClientProfile < ActiveRecord::Base
   def has_prepaid_membership?
     sql = case ::RAILS_ENV
     when 'development'
-      "(Code = 'VY' OR Code = 'VY+' OR Code = 'V1M' OR Code = 'V1W') AND CType != 1 AND client_no = ? AND Last_Mdt > ?"
+      "(Code = 'VY' OR Code = 'VY+' OR Code = 'V1M' OR Code = 'V1W') AND CType != ? AND client_no = ? AND Last_Mdt > ?"
     when 'production'
-      "([Code] = 'VY' OR [Code] = 'VY+' OR [Code] = 'V1M' OR [Code] = 'V1W') AND CType != 1 AND [client_no] = ? AND [Last_Mdt] > ?"
+      "([Code] = 'VY' OR [Code] = 'VY+' OR [Code] = 'V1M' OR [Code] = 'V1W') AND CType != ? AND [client_no] = ? AND [Last_Mdt] > ?"
     end
-    mem_trans = Helios::Transact.find(:all, :conditions => [sql, self.id, Time.now-47088000])
+    mem_trans = Helios::Transact.find(:all, :conditions => [sql, 1, self.id, Time.now-47088000])
     lasting = {
       'VY'  => Time.now-36720000, # 425 days
       'VY+' => Time.now-47088000, # 545 days
