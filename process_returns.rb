fae4b05bb48233c700d6643220892aacefac8696
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2008-01-04 16:07:45 +0000

    commented for safety in testing.
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@1003 55dd9f42-8528-0410-b227-b9e89acb4f78

--- process_returns.rb
+++ process_returns.rb
@@ -45,34 +45,34 @@ ARGV[0] != '--limited' && ARGV[0] != '--revert-helios' && step("Reading return f
   
   files.each do |file|
     step("Reading #{file} into MySQL") do
-      clients = {}
-      invalids = []
-      header = nil
-      CSV::Reader.parse(File.open(@path+file, 'rb').map {|l| l.gsub(/[\n\r]+/, "\n")}.join) do |row|
-        res = GotoResponse.new(row)
-        unless header
-          header = row
-          next
-        end
-        if header.join(',') == 'MerchantID,FirstName,LastName,CustomerID,Amount,SentDate,SettleDate,TransactionID,TransactionType,Status,Description'
-          invalid = res.invalid?
-          # if !clients.has_key?(res.client_id) #Don't need to check for duplicates here, it's handled simply by checking if the status has changed since a previous recording.
-          if res.client
-            # Duplicate: First should always be an accept.. so delete the accept transaction
-            #     and clear it from the goto_transaction so that the new response can be run.
-            report "Copying client #{res.inspect} to MySQL..." if rand(20) == 15
-            res.record_to_client!
-          else
-            # invalid: client doesn't exist
-            invalid = "Client doesn't exist"
-          end
-          clients[res.client_id] = res
-          invalids << "Client ##{res.client_id}: #{invalid}" if invalid
-        else
-          invalids = ["!!! INVALID RETURN FILE !!!"]
-        end
-      end
-      report "Problems:\n\t#{invalids.join("\n\t")}"
+      # clients = {}
+      # invalids = []
+      # header = nil
+      # CSV::Reader.parse(File.open(@path+file, 'rb').map {|l| l.gsub(/[\n\r]+/, "\n")}.join) do |row|
+      #   res = GotoResponse.new(row)
+      #   unless header
+      #     header = row
+      #     next
+      #   end
+      #   if header.join(',') == 'MerchantID,FirstName,LastName,CustomerID,Amount,SentDate,SettleDate,TransactionID,TransactionType,Status,Description'
+      #     invalid = res.invalid?
+      #     # if !clients.has_key?(res.client_id) #Don't need to check for duplicates here, it's handled simply by checking if the status has changed since a previous recording.
+      #     if res.client
+      #       # Duplicate: First should always be an accept.. so delete the accept transaction
+      #       #     and clear it from the goto_transaction so that the new response can be run.
+      #       report "Copying client #{res.inspect} to MySQL..." if rand(20) == 15
+      #       res.record_to_client!
+      #     else
+      #       # invalid: client doesn't exist
+      #       invalid = "Client doesn't exist"
+      #     end
+      #     clients[res.client_id] = res
+      #     invalids << "Client ##{res.client_id}: #{invalid}" if invalid
+      #   else
+      #     invalids = ["!!! INVALID RETURN FILE !!!"]
+      #   end
+      # end
+      # report "Problems:\n\t#{invalids.join("\n\t")}"
     end
   end
 end
