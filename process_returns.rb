8da87b8ae254bc1ccbf923d29021e69eb5b6bbd6
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2008-01-06 21:18:00 +0000

    Re-enabling process_returns to run its powerhouse code!
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/branches/malibu@1031 55dd9f42-8528-0410-b227-b9e89acb4f78

--- process_returns.rb
+++ process_returns.rb
@@ -45,34 +45,34 @@ end
   
   files.each do |file|
     step("Reading #{file} into MySQL") do
-      # clients = {}
-      # invalids = []
-      # header = nil
-      # CSV::Reader.parse(File.open(@path+file, 'rb').map {|l| l.gsub(/[\n\r]+/, "\n")}.join) do |row|
-      #   res = GotoResponse.new(@batch.id, row)
-      #   unless header
-      #     header = row
-      #     next
-      #   end
-      #   if header.join(',') == 'MerchantID,FirstName,LastName,CustomerID,Amount,SentDate,SettleDate,TransactionID,TransactionType,Status,Description'
-      #     invalid = res.invalid?
-      #     # if !clients.has_key?(res.client_id) #Don't need to check for duplicates here, it's handled simply by checking if the status has changed since a previous recording.
-      #     if res.client
-      #       # Duplicate: First should always be an accept.. so delete the accept transaction
-      #       #     and clear it from the goto_transaction so that the new response can be run.
-      #       report "Copying client #{res.inspect} to MySQL..." if rand(20) == 15
-      #       res.record_to_client!
-      #     else
-      #       # invalid: client doesn't exist
-      #       invalid = "Client doesn't exist"
-      #     end
-      #     clients[res.client_id] = res
-      #     invalids << "Client ##{res.client_id}: #{invalid}" if invalid
-      #   else
-      #     invalids = ["!!! INVALID RETURN FILE !!!"]
-      #   end
-      # end
-      # report "Problems:\n\t#{invalids.join("\n\t")}"
+      clients = {}
+      invalids = []
+      header = nil
+      CSV::Reader.parse(File.open(@path+file, 'rb').map {|l| l.gsub(/[\n\r]+/, "\n")}.join) do |row|
+        res = GotoResponse.new(@batch.id, row)
+        unless header
+          header = row
+          next
+        end
+        if header.join(',') == 'MerchantID,FirstName,LastName,CustomerID,Amount,SentDate,SettleDate,TransactionID,TransactionType,Status,Description'
+          invalid = res.invalid?
+          # if !clients.has_key?(res.client_id) #Don't need to check for duplicates here, it's handled simply by checking if the status has changed since a previous recording.
+          if res.client
+            # Duplicate: First should always be an accept.. so delete the accept transaction
+            #     and clear it from the goto_transaction so that the new response can be run.
+            report "Copying client #{res.inspect} to MySQL..." if rand(20) == 15
+            res.record_to_client!
+          else
+            # invalid: client doesn't exist
+            invalid = "Client doesn't exist"
+          end
+          clients[res.client_id] = res
+          invalids << "Client ##{res.client_id}: #{invalid}" if invalid
+        else
+          invalids = ["!!! INVALID RETURN FILE !!!"]
+        end
+      end
+      report "Problems:\n\t#{invalids.join("\n\t")}"
     end
   end
 end
@@ -82,22 +82,21 @@ end
   trans = GotoTransaction.find(:all, :conditions => ['batch_id=? AND ((goto_invalid IS NOT NULL AND !(goto_invalid LIKE ?)) OR (status IS NOT NULL AND status != ?))', @batch.id, '%'+[].to_yaml+'%', ''], :order => 'id ASC')
   report "There are #{trans.length} completed transactions to record to Helios."
   # Filter to those that don't have a transaction_id
-  # to_record = trans.reject {|t| !t.transaction_id.blank? && t.transaction_id != 0}
+  to_record = trans.reject {|t| !t.transaction_id.blank? && t.transaction_id != 0}
   # FOR TESTING PURPOSES! (also tested on 20000002)
   # to_record = to_record[0..19]
   # * * * *
-  # report "Of these, #{trans.length} have yet to be recorded to Helios."
+  report "Of these, #{to_record.length} have yet to be recorded to Helios."
 
-# Commented just for safety in testing..
-  # counts = {:accepted => 0, :declined => 0, :invalid => 0}
-  # trans.each do |tran|
-  #   step("Client ##{tran.client_id}") do
-  #     counts[!tran.goto_invalid.to_a.blank? ? :invalid : (tran.status == 'G' ? :accepted : :declined)] += 1
-  #     # The payment could be accepted, declined, or invalid.
-  #     tran.record_to_helios!
-  #   end
-  # end
-  # report "#{counts[:accepted]} Accepted, #{counts[:declined]} Declined, #{counts[:invalid]} Invalid"
+  counts = {:accepted => 0, :declined => 0, :invalid => 0}
+  trans.each do |tran|
+    step("Client ##{tran.client_id}") do
+      counts[!tran.goto_invalid.to_a.blank? ? :invalid : (tran.status == 'G' ? :accepted : :declined)] += 1
+      # The payment could be accepted, declined, or invalid.
+      tran.record_to_helios!
+    end
+  end
+  report "#{counts[:accepted]} Accepted, #{counts[:declined]} Declined, #{counts[:invalid]} Invalid"
 end
 
 ARGV.include?('--revert-helios') && step("Reverting everything recorded to Helios") do
