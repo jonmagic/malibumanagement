ca84f3b99c34ce21c37d1cef7c3bffd55f55cfb0
Daniel Parker <daniel@Daniel-Parkers-MacBook.local>
2009-09-21 17:13:45 -0400

    Deleted the batch_submitted migration, did it manually.

--- db/migrate/040_lengthen_batch_submitted.rb
+++
@@ -1,9 +0,0 @@
-class LengthenBatchSubmitted < ActiveRecord::Migration
-  def self.up
-    change_column :eft_batches, :submitted, :text
-  end
-  
-  def self.down
-    # change_column :eft_batches, :submitted, :string, :default => "--- {}\n\n"
-  end
-end
