c1deb59d4339ccb7c5b54c990b99583f03f22784
Daniel Parker <daniel@Daniel-Parkers-MacBook.local>
2009-09-21 16:20:49 -0400

    Fixed jquery problem.

--- public/javascripts/eft_uploader.js
+++ public/javascripts/eft_uploader.js
@@ -13,30 +13,27 @@ var repeat_billing = function(for_month, incoming_path){
   var $billing = jQuery('#time_to_bill');
   var $billing_files = jQuery('#time_to_bill ul');
   // 2) Call the action
-  jQuery.getJSON({
-    url : (incoming_path ? '/malibu/eft/submit_payments?for_month='+for_month+'&incoming_path='+incoming_path : '/malibu/eft/submit_payments?for_month='+for_month),
-    success : function(data){
-      var key, that_remain=0, store, type;
-      // 3) Integrate results
-      for(key in data){
-        if(key == 'error'){
-          alert(data[key]);
+  jQuery.getJSON((incoming_path ? '/malibu/eft/submit_payments?for_month='+for_month+'&incoming_path='+incoming_path : '/malibu/eft/submit_payments?for_month='+for_month), function(data){
+    var key, that_remain=0, store, type;
+    // 3) Integrate results
+    for(key in data){
+      if(key == 'error'){
+        alert(data[key]);
+      }else{
+        // type = data[key].split('--')[1];
+        // store = data[key].split('--')[0];
+        if($billing_files.find("#upload_"+key).length===0){
+          $billing_files.append("<li>"+key+"<span id='upload_status_"+key+"'>"+data[key]+"</span></li>");
         }else{
-          // type = data[key].split('--')[1];
-          // store = data[key].split('--')[0];
-          if($billing_files.find("#upload_"+key).length===0){
-            $billing_files.append("<li>"+key+"<span id='upload_status_"+key+"'>"+data[key]+"</span></li>");
-          }else{
-            $billing_files.find("#upload_status_"+key).text(data[key]);
-          }
-          if(data[key].split(' ')[0] == "Failed") that_remain = that_remain + 1; // if first word is "Failed"
+          $billing_files.find("#upload_status_"+key).text(data[key]);
         }
+        if(data[key].split(' ')[0] == "Failed") that_remain = that_remain + 1; // if first word is "Failed"
       }
-      // 4) repeat from #2 if some remain
-      if(that_remain > 0){
-        $billing.find('h3').text("Retry uploading "+that_remain+" files to DCAS...");
-        repeat_billing(for_month);
-      }
+    }
+    // 4) repeat from #2 if some remain
+    if(that_remain > 0){
+      $billing.find('h3').text("Retry uploading "+that_remain+" files to DCAS...");
+      repeat_billing(for_month);
     }
   });
 };
