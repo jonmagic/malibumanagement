26c24b5e4144cd9e7127bb9a6a10a044d01a58cf
Jonathan Hoyt <jonmagic@gmail.com>
2011-08-08 15:27:14 -0400

    Fixing master inventory report so it handles nil for product descriptions.

--- app/controllers/master_inventory_price_list_items_controller.rb
+++ app/controllers/master_inventory_price_list_items_controller.rb
@@ -5,11 +5,11 @@ class MasterInventoryPriceListItemsController < ApplicationController
     if params[:master_inventory_report_id]
       # renders a page for editing only missing items
       @items = MasterInventoryPriceListItem.items_missing_prices(params[:master_inventory_report_id])
-      @items = @items.sort_by(&:description)
+      @items = @items.sort_by {|item| item.description.to_s }
     else
       # renders a page for editing the entire list
       @items = MasterInventoryPriceListItem.find(:all)
-      @items = @items.sort_by(&:description)
+      @items = @items.sort_by {|item| item.description.to_s }
       render :layout => 'master_inventory_price_list'
     end
   end
